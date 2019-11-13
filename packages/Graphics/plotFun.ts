4b7820203cbf5fdafc5118fe656e39dc360a037b
pireddag <48107401+pireddag@users.noreply.github.com>
2019-11-13 21:50:39 +0100

    Place package inside "package" directory

---
+++ packages/Graphics/plotFun.ts
@@ -0,0 +1,15 @@
+<TeXmacs|1.99.11>
+
+<style|generic>
+
+<\body>
+  <use-module|(graphics plotting plotFun)>
+
+  <assign|plotFun|<macro|fun|range|<extern|plotFun|<arg|fun>|<arg|range>>>>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
