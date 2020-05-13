88e6b74a7374b4754ea907e429a1da96e8cfc46d
pireddag <48107401+pireddag@users.noreply.github.com>
2020-05-14 00:27:50 +0200

    Package with macro for the Scheme files

---
+++ packages/Graphics/plotFun_working_05.ts
@@ -0,0 +1,15 @@
+<TeXmacs|1.99.12>
+
+<style|<tuple|generic|old-dots>>
+
+<\body>
+  <use-module|(graphics plotting_working_05 plotFun)>
+
+  <assign|plotFun|<macro|definitionFile|<extern|plotFun|<arg|definitionFile>>>>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|preamble|true>
+  </collection>
+</initial>
