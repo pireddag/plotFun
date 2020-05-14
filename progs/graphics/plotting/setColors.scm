30a7f10e5e14eab853b56c0439ad1ea9beeb802e
pireddag <48107401+pireddag@users.noreply.github.com>
2020-05-14 13:25:17 +0200

    Update color list

--- progs/graphics/plotting/setColors.scm
+++ progs/graphics/plotting/setColors.scm
@@ -2,10 +2,18 @@
 (define (cycleList lst)
   (append (cdr lst) (list (car lst))))
 
-(define colorList
-  '("blue" "red" "green"))
+;;(define colorList
+;;  '("blue" "red" "green"))
+;; gnuplot colors
+;; http://www.gnuplot.info/ReleaseNotes_5_0_7.html
+;; red/green/blue/magenta/cyan/yellow
+;; (define colorList
+;;   '("blue" "red" "green" "magenta" "cyan" "yellow"))
 
-;; make a list of colors as long as the list of functions by cylcing through colorList
+(define colorList
+  '("blue" "red" "green" "#007f00" "cyan" "yellow"))
+
+;; make a list of colors as long as the list of functions by cycling through colorList
 (define (colorListForThisPlot cList gList)
   (if (null? gList)
       '()
