63a1470bc813572929b13c170c1044edee1712ce
pireddag <48107401+pireddag@users.noreply.github.com>
2020-05-19 17:51:02 +0200

    Delete test input with "only-functions" syntax
    Replace it with test input that includes annotations

--- fundefs_05.scm
+++
@@ -1,8 +0,0 @@
-(list
- `(("function" . ,(lambda (x) ( - (expt x 2) 2.)))
-   ("range" . ,(list -2. 2.)))
- `(("function" . ,(lambda (x) ( - 2. (expt x 2) )))
-   ("range" . ,(list -1. 1.)))
- )
-
-
