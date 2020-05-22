848432ad8ea309a87f5ad47e8e78728debd765a0
pireddag <48107401+pireddag@users.noreply.github.com>
2020-05-22 15:47:29 +0200

    Reorganization of directories

---
+++ plotFun/progs/setColors.scm
@@ -0,0 +1,28 @@
+(texmacs-module (setColors))
+
+;; cycle a list
+(tm-define (cycleList lst)
+	   	   (:secure)
+  (append (cdr lst) (list (car lst))))
+
+;;(define colorList
+;;  '("blue" "red" "green"))
+;; gnuplot colors
+;; http://www.gnuplot.info/ReleaseNotes_5_0_7.html
+;; red/green/blue/magenta/cyan/yellow
+;; (define colorList
+;;   '("blue" "red" "green" "magenta" "cyan" "yellow"))
+
+(tm-define colorList
+	   	   (:secure)
+  '("blue" "red" "green" "#007f00" "cyan" "yellow"))
+
+;; make a list of colors as long as the list of functions by cycling through colorList
+(tm-define (colorListForThisPlot cList gList)
+	   	   (:secure)
+  (if (null? gList)
+      '()
+      (cons (car cList) (colorListForThisPlot (cycleList cList) (cdr gList)))))
+
+;; (map (lambda (x y) (+ x y)) '(1 2 3) '(3 2 1))
+  
